bfa8e1bf47f9a9ba31f28ed99764450a07c8b43f
Ryan Lelek <ryanlelek@gmail.com>
2014-06-20 16:09:52 -0700

    Adding basic module with tests

---
+++ app/module.basic.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+// Exports
+module.exports = {
+  add      : add,
+  multiply : multiply
+};
+
+function add (a, b) {
+  return a + b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
---
+++ test/unit/module.basic.test.js
@@ -0,0 +1,32 @@
+
+'use strict';
+
+// Modules
+require('should');
+
+// Subject
+var basic = require('../../app/module.basic.js');
+
+describe('Module - Basic', function () {
+
+  describe('.add()', function () {
+
+    it('should return the sum when given two numbers', function () {
+      basic.add(2, 5).should.equal(7);
+      basic.add(10, 42).should.equal(52);
+      basic.add(2, 0).should.not.equal(20);
+    });
+
+  });
+
+  describe('.multiply()', function () {
+
+    it('should return the product when given two numbers', function () {
+      basic.multiply(2, 5).should.equal(10);
+      basic.multiply(10, 42).should.equal(420);
+      basic.multiply(2, 0).should.not.equal(2);
+    });
+
+  });
+
+});
